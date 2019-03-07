d4acc4f5ec0064bb79b403ca5a68d3afb0125dad
Quang Vinh <ngoquangvinh1997tg@gmail.com>
2019-03-07 13:36:31 +0700

    ban clone

--- 1.js
+++ 1.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded',function(){
 	var button = document.querySelectorAll('ul.icon-bar li');
 	var slide = document.querySelectorAll('ul.slide-bar li');
 	var default1 = document.getElementsByClassName('default-display')[0];
+
+
+
+	
 	for(let i = 0; i<=button.length -1; i++){
 		button[i].addEventListener('click',function(){
 			/*Xoá class hiển thị mặc định*/
